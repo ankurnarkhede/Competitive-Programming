08f15b215c9da38d198f802efe31c4365392a131
smartankur4u <ankurnarkhede@gmail.com>
2018-03-30 00:35:01 +0530

    some problems solved...

--- he-algo-search-MonkandSpecialInteger.py
+++ he-algo-search-MonkandSpecialInteger.py
@@ -11,10 +11,10 @@ for i in range(0,n,+1):
     sub_array=[]
     sub_array_sum=[]
     for j in range (0, n-i, +1):
-        sub_array.append(a[j:j+i+1])
-        sub_array_sum.append (sum(sub_array[j]))
+        # sub_array.append(a[j:j+i+1])
+        sub_array_sum.append (sum(a[j:j+i+1]))
 
-    if(max(sub_array_sum)<x):
+    if(max(sub_array_sum)<=x):
         k+=1
 
 print(k)
--- temp.py
+++ temp.py
@@ -14,10 +14,12 @@ for i in range(0,n,+1):
     sub_array=[]
     sub_array_sum=[]
     for j in range (0, n-i, +1):
-        sub_array.append(a[j:j+i+1])
-        print(sub_array)
-        sub_array_sum.append (sum(sub_array[j]))
-    print(sub_array_sum)
+        # sub_array.append(a[j:j+i+1])
+        # print(sub_array)
+
+        # sub_array.append(a[j:j+i+1])
+        sub_array_sum.append (sum(a[j:j+i+1]))
+        print(sub_array_sum)
 
     print(max(sub_array_sum))
     if(max(sub_array_sum)<=x):
