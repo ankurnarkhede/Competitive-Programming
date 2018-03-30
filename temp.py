3dd3d01be98d457587195a4096bb33a64f583d63
smartankur4u <ankurnarkhede@gmail.com>
2018-03-30 22:41:47 +0530

    some problems solved...

--- temp.py
+++ temp.py
@@ -6,11 +6,10 @@ import sys
 
 n, x = (map (int, sys.stdin.readline ().strip ().split (' ')))
 a = (list (map (int, sys.stdin.readline ().strip ().split (' '))))
-
 k=0
 
 for i in range(0,n,+1):
-    print('=======================')
+    print(i,'=======================')
     sub_array=[]
     sub_array_sum=[]
     for j in range (0, n-i, +1):
@@ -19,12 +18,12 @@ for i in range(0,n,+1):
 
         # sub_array.append(a[j:j+i+1])
         sub_array_sum.append (sum(a[j:j+i+1]))
-        print(sub_array_sum)
+        # print(sub_array_sum)
 
-    print(max(sub_array_sum))
+    # print(max(sub_array_sum))
     if(max(sub_array_sum)<=x):
         k+=1
-        print('k=',k)
+        # print('k=',k)
 
 print(k)
 
