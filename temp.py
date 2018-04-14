17ac8f9d2b4fdd90f9ccab14f5e6cc5b022c77af
smartankur4u <ankurnarkhede@gmail.com>
2018-04-14 07:48:52 +0530

    some problems solved...

--- temp.py
+++ temp.py
@@ -1,39 +1,53 @@
 
+# 6
+# 3 6 1 1
+# .@@..@
+# .....@
+# @.@.@@
+
+
 import sys
 
-n = int(input ())  # No. of test cases
+import numpy as np
 
-for _ in range (n):
+n = int(input()) # NO. of testcases
 
-    # Each test case
+def is_possible(a, i, j):
+	'''
+	a is ndarray and i and j are coordinates to check
+	'''
+	p,q,r,s = a[:i,:j], a[:i, j:], a[i:, :j], a[i:, j:]
 
-    A = int (input ())
-    if A < 9:
-        A = 9
+	n = np.count_nonzero(p)
+	print(n)
+	if np.count_nonzero(q) == n and np.count_nonzero(r) == n and np.count_nonzero(s) == n:
+		return True
+	else:
+		return False
 
-    if A % 3 != 0:
-        A += (3 - (A % 3))
+for _ in range(n):
+	r,c,h,v = [ int(i) for i in input().strip().split() ]
+	waffle_array = []
+	for line in range(r):
+		t = [] # One row of the array
+		for character in input().strip():
+			if character == '.':
+				t.append(0)
+			if character == '@':
+				t.append(1)
+		waffle_array.append(t)
 
-    num_targets = int (A / 3)
+	waffle_array = np.array(waffle_array)
+	if np.count_nonzero(waffle_array) == 0:
+		print("Case #{}: POSSIBLE".format(_+1))
+	else:
+		flag = False
+		for i in range(1, r+1):
+			for j in range(1, c+1):
+				if is_possible(waffle_array, i, j):
+					print("Case #{}: POSSIBLE".format(_+1))
+					flag = True
+					break
 
-    i = 2
-    j = 2
-    curr_dug = set ()
-    end_test_case = False  # Flag variable for flow control
-    while j != num_targets + 2:
-        print ("{} {}".format (i, j))
-        sys.stdout.flush ()
-        hole_at = tuple ([int (i) for i in input ().strip ().split ()])
-        if hole_at == (-1, -1):
-            exit ()
-        if hole_at == (0, 0):
-            end_test_case = True
-            break
-
-        curr_dug.add (hole_at)
-        if len (curr_dug) == 9:
-            j += 1
-            curr_dug = set ()
-
-    if end_test_case:
-        continue
+		if not flag:
+			print("Case #{}: IMPOSSIBLE".format(_+1))
