017db9dd97a38e051238cedfe9c936f897d0d8e9
smartankur4u <ankurnarkhede@gmail.com>
2018-05-15 14:11:56 +0530

    some problems solved...

--- he-ds-queue-MonkandChamberofSecrets.py
+++ he-ds-queue-MonkandChamberofSecrets.py
@@ -10,30 +10,21 @@ from collections import deque
 n, x = (map (int, stdin.readline ().strip ().split (' ')))
 a = (list (map (int, stdin.readline ().strip ().split (' '))))
 
-# a_indexed=np.array(deque())
-# a_dequeued=np.array(deque())
 a_indexed = deque ()
 
 for i, j in enumerate (a):
-    # a_indexed=np.append(a_indexed,[j,i+1])
     a_indexed.append ([j, i + 1])
-print ('a_indexed={}'.format (a_indexed))
 
 for i in range (0, x, +1):
     a_dequeued = deque ()
-    print ('============{}==========='.format (i))
-    print ('initial a_indexed={}'.format (a_indexed))
-    print ('initial a_dequeued={}'.format (a_dequeued))
 
     for j in range (0, min (x, len (a_indexed))):
         a_dequeued.append (a_indexed.popleft ())
 
-    print ('a_dequeued=', a_dequeued)
     max_this_time = max (a_dequeued, key=lambda x: x[0])
-    print ('max this time={}'.format (max_this_time))
     max_index = max_this_time[1]
 
-    print ('Index={}'.format (max_index))
+    print(max_index, end=' ')
 
     del (a_dequeued[a_dequeued.index(max_this_time)])
 
