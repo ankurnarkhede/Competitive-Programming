cf198db027ef884b5af5d2f8d96c4a5b4a7a6eba
smartankur4u <ankurnarkhede@gmail.com>
2018-05-25 10:20:51 +0530

    some problems solved...

--- temp.py
+++ temp.py
@@ -1,72 +1,25 @@
-from sys import stdin
-line_index = -1
-lines = stdin.readlines ()
+n = int (input ())
+a = list (map (int, input ().split ()))
+M = max (a) + 1
+cnt = [0 for _ in range (M + 1)]
+for el in a:
+    cnt[el] += 1
+print(cnt)
 
-def get_line():
-    global lines, line_index
-    line_index += 1
-    return lines[line_index]
-
-def binary_search(arr, val):
-    low = 0
-    high = len (arr) - 1
-    while low < high:
-        mid = (low + high) // 2
-        if arr[mid] > val:
-            high = mid
-    else:
-        low = mid + 1
-    if arr[low] > val:
-        return arr[low]
-    return -1
-
-def generate_smallest(arr):
-    stack = [arr[0]]
-    for i in range (1, len (arr)):
-        val = arr[i]
-        if val < stack[-1]:
-            stack.append (val)
-        else:
-            stack.append (stack[-1])
-    return stack
-
-def get_ans(army, stack):
-    if not stack:
-        return "NO"
-    elem = stack[-1]
-    for i in range (1, len (army)):
-    elem = binary_search (army[i], elem)
-    if elem == -1:
-    return "NO"
-    return "YES"
-
-def main():
-    n = int (get_line ())
-    army = []
-    for _ in range (n):
-    row = [int (x) for x in get_line ().split ()[1:]]
-    army.append (row)
-    q = int (get_line ())
-    stack = generate_smallest (army[0])
-    for _ in range (q):
-    query = [int (x) for x in get_line ().split ()]
-    if query[0] == 1:
-    row, val = query[1:]
-    row -= 1
-    army[row].append (val)
-    if row == 0:
-    if val < stack[-1]:
-    stack.append (val)
-    else:
-    stack.append (stack[-1])
-    elif query[0] == 0:
-    row = query[1]
-    row -= 1
-    army[row].pop ()
-    if row == 0:
-    stack.pop ()
-    else:
-    print (get_ans (army, stack))
+def gcd(a, b):
+    if min (a, b) == 0:
+        return max (a, b)
+    return gcd (b, a % b)
 
 
-main ()
+dv = [0 for _ in range (M + 1)]
+
+for num in range (1, M + 1):
+    for nc in range (num, M + 1, num):
+        dv[num] += cnt[nc]
+print(dv)
+
+for _ in range (int (input ())):
+    p, q = map (int, input ().split ())
+    g = gcd (p, q)
+    print ((dv[p] if 0 < p <= M else 0) + (dv[q] if 0 < q <= M else 0) - (dv[p * q // g] if 0 < p * q // g <= M else 0))
