cf99daef2569286c275a2837e65760b9ef6d4fbc
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-09-25 04:59:54 +0530

    some problems solved...

---
+++ he-city-travel.py
@@ -0,0 +1,30 @@
+# https://www.hackerearth.com/challenge/competitive/september-circuits-18/algorithm/city-travel-59bad87f/
+
+# 21 5 2
+# 2 4
+# 4 8
+
+import sys
+
+def main():
+    dict={}
+    s,x,n= (map (int, sys.stdin.readline ().strip ().split (' ')))
+
+    for i in range(n):
+        t,y = (map (int, sys.stdin.readline ().strip ().split (' ')))
+        dict[t]=y
+
+    day=0
+    while(s>0):
+        if(dict.get(day,0)):
+            s-=dict.get(day)
+        else:
+            s-=x
+        day+=1
+
+    print(day)
+
+
+if __name__ == "__main__":
+    main ()
+
