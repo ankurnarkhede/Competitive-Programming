41c7af1665963f60873556e3267579ef1f2dbcf3
Ankur Narkhede <ankurnarkhede@gmail.com>
2019-01-19 14:21:31 +0530

    ...

---
+++ LazyBartender.py
@@ -0,0 +1,45 @@
+import sys
+import operator
+
+n = int(input())
+c = int(input())
+
+dict = {}
+
+drinks = []
+
+for i in range(c):
+    a = (list(map(int, input().strip().split(' '))))
+
+    drinks.append(a[1:])
+
+    for j in range(1, len(a)):
+        try:
+            dict[a[j]] += 1
+
+        except(KeyError):
+            dict[a[j]] = 1
+
+# print("drinks=", drinks)
+# print("dict=", dict)
+
+sorted_d = sorted(dict.items(), key=operator.itemgetter(1), reverse=True)
+# print("SORTED=", sorted_d)
+
+count = 0
+deleted = True
+while (deleted):
+    deleted = False
+    nos = sorted_d[0][0]
+    del (sorted_d[0])
+    for k in range(len(drinks)):
+        if nos in drinks[k]:
+            drinks[k] = [-1]
+            deleted = True
+
+    if (deleted):
+        count += 1
+
+    # print("DRINKS=", drinks, "count=", count)
+
+print(count)
